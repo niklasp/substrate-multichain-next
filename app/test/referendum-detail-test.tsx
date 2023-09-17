d31c4b1074ccf5d0dc7029fb4c34e6f4e858fcc5
Niklas P <contact@niklasplessing.net>
2023-09-18 00:14:36 +0200

    wip

--- app/test/referendum-detail-test.tsx
+++ app/test/referendum-detail-test.tsx
@@ -19,6 +19,7 @@ import { ReferendumLinks } from "../vote/components/referendum-links";
 import ReferendumVoteButtons from "../vote/components/referendum-vote-buttons";
 import { ReferendumUserInfoCard } from "../vote/components/referendum-user-info";
 import ReferendumCountdownCard from "../vote/components/referendum-countdown-card";
+import { UIReferendum, UITrack } from "../vote/types";
 
 export const ReferendumDetailLoading = ({
   isLoaded,
